6d6ad06f4a019c30e4af4a42eb0942e50bf86488
morthenn <minerty@gmail.com>
2017-01-05 14:44:27 +0100

    strategy generation added2

--- src/main/java/pl/michalgubanski/model/Patient.java
+++ src/main/java/pl/michalgubanski/model/Patient.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
-@Table(name = "patientList")
+@Table(name = "patients")
 public class Patient {
     private static final String SEPARATOR = "\t";
 
